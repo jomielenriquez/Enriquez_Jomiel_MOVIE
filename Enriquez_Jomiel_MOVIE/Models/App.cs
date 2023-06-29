d294f78841ce5ca736a25a9b43108462e9dd5108
Jomiel <enriquez.jliquigan@gmail.com>
2023-06-29 15:02:36 +0800

    Update Final

---
+++ Enriquez_Jomiel_MOVIE/Models/App.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Enriquez_Jomiel_MOVIE.Models
+{
+    public class App
+    {
+        public IEnumerable<TBL_MOVIES> MovieList { get; set; }
+        public TBL_MOVIES SelectedMovie { get; set; }
+        public IEnumerable<TBL_CATEGORIES> CategoryList { get; set; }
+        public List<string> AreChecked { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
